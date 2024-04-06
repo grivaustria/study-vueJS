7cdaec79efe9991cc759e357374f91b4f21f2ef9
Gabriel Austria <grivaustria@gmail.com>
2024-04-06 22:37:34 +0800

    Added: Understanding the use of "methods" in Vue apps

--- _3-Basics-and-Core-Concepts/app.js
+++ _3-Basics-and-Core-Concepts/app.js
@@ -4,6 +4,18 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course and master Vue!',
             vueLink: 'https://vuejs.org',
         }
+    },
+    
+
+    methods: {
+        outputGoal: () => {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5){
+                return 'Learn Vue!';
+            } else {
+                return 'You have finally mastered Vue!';
+            }
+        }
     }
 });
 
