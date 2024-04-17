8e2ba1ff22417c8c41e534a64c6b1c98f682360c
Gabriel Austria <grivaustria@gmail.com>
2024-04-17 19:28:15 +0800

    Two-way Binding Complete

--- _3-Basics-and-Core-Concepts/_3_3-Two-Way-Binding/app.js
+++ _3-Basics-and-Core-Concepts/_3_3-Two-Way-Binding/app.js
@@ -1,29 +1,46 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        counter: 0,
-        name: ''
-      };
-    },
-    methods: {
-      // setName(event, lastName) {
-      //   this.name = event.target.value + ' ' + lastName;
-      // },
-      setName(event) {
-        this.name = event.target.value;
-      },
-      add(num) {
-        this.counter = this.counter + num;
-      },
-      reduce(num) {
-        this.counter = this.counter - num;
-        // this.counter--;
-      },
-      resetInput(){
-        this.name = '';
-      }
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      fullname: '',
     }
-  });
-  
-  app.mount('#events');
-  
+  },
+
+  // computed: {
+  //   fullname(){
+  //     console.log('Running again...');
+  //     if (this.name === '') {
+  //       return '';
+  //     }
+  //     return this.name + ' ' + 'Austria';
+  //   }
+
+  // },
+  watch:{
+    name(value){
+      this.fullname = value + ' ' + 'Austria';
+    }
+  },
+
+  methods: {
+    // setName(event, lastName) {
+    //   this.name = event.target.value + ' ' + lastName;
+    // },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = '';
+    }
+  }
+});
+
+app.mount('#events');
